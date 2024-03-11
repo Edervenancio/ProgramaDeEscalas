4f41ce5ee66dd165a78e0b3f1109479a0b714905
Eder venancio <ederpvenancio@gmail.com>
2024-03-11 19:41:35 -0300

    first commit

---
+++ src/main/java/com/example/ProgramaDeEscalas/ProgramaDeEscalasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgramaDeEscalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaDeEscalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaDeEscalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgramaDeEscalas/ProgramaDeEscalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgramaDeEscalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaDeEscalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
